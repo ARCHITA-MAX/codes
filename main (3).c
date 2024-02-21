9f20b98c7c6774ffbe1fc5c3c7b734d7f722515b
ARCHITA-MAX <157284709+ARCHITA-MAX@users.noreply.github.com>
2024-02-22 00:28:47 +0530

    Add files via upload

---
+++ main (3).c
@@ -0,0 +1,18 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include<stdio.h>
+#include<string.h>
+ int main()
+ {
+     char str[20];
+     printf("Enter the string ");
+     fgets(str,20,stdin);
+     puts(str);
+     return 0;
+ }
