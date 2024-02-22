68d0589ab5f69f483967a63eb91a18f689caa0d6
ARCHITA-MAX <157284709+ARCHITA-MAX@users.noreply.github.com>
2024-02-22 17:06:34 +0530

    Add files via upload

---
+++ main (11).c
@@ -0,0 +1,44 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+int chkSubset(int arr1 , int no1, int arr2, int no2) 
+{
+    int i, j;
+    for (i = 0; i <no1; i++)
+    {
+        for (j = 0; j <no2; j++)
+        {
+            if (arr2[i] == arr1[j])
+                break;
+        }
+        if (j == no1)
+            return 0;
+    }
+    return 1; 
+}
+
+int main() 
+{
+    int arr1[20],arr2[20];
+    int i;
+    printf("enter the value of n1 & n2");
+    scanf("%d%d",&n1,&n2);
+    pritnf("enter the elements of  arrays1");
+    for(int i=0;i<n1;i++)
+    scanf("%d",&arr[i]);
+    printf("enter the elements of array2");
+    for(int j=0;j<n2;i++)
+    scanf("%d";&arr[j])
+    if (chkSubset(arr1, no1, arr2, no2))
+        printf("The second array is a subset of the first array");
+    else
+        printf("The second array is not a subset of the first array");
+
+    return 0;
+}
